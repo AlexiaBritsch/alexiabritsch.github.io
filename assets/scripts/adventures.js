781e1b5998bb912a2a2ba8a814e16a79f4e04932
AlexiaBritsch <42224454+AlexiaBritsch@users.noreply.github.com>
2019-04-29 22:59:58 -0700

    Finished Project

---
+++ assets/scripts/adventures.js
@@ -0,0 +1,50 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+ }
+
+function open1() {
+  document.getElementById("blowup1").style.display = "flex";
+}
+
+function close1() {
+  document.getElementById("blowup1").style.display = "none";
+
+}
+
+function open2() {
+  document.getElementById("blowup2").style.display = "flex";
+}
+
+function close2() {
+  document.getElementById("blowup2").style.display = "none";
+
+}
+
+function open3() {
+  document.getElementById("blowup3").style.display = "flex";
+}
+
+function close3() {
+  document.getElementById("blowup3").style.display = "none";
+
+}
+function open4() {
+  document.getElementById("blowup4").style.display = "flex";
+}
+
+function close4() {
+  document.getElementById("blowup4").style.display = "none";
+
+}
+function open5() {
+  document.getElementById("blowup5").style.display = "flex";
+}
+
+function close5() {
+  document.getElementById("blowup5").style.display = "none";
+
+}
